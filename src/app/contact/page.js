cb4018a43c572c0c122c6214ef42f93174c75ff7
sunshinevendetta <57574173+sunshinevendetta@users.noreply.github.com>
2025-01-23 17:38:34 -0600

    v1

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -6,7 +6,7 @@ export default function WarningModal() {
     <div className="bg-black p-6 rounded shadow-lg max-w-md w-full text-center">
       <h2 className="text-2xl font-bold mb-4 text-white">Warning</h2>
       <p className="mb-4 text-white">
-        This site is sun setting for lack of payment on behalf from DFB (Decentralized Future Builders)
+        This site is sun setting for lack of payment.
       </p>
     </div>
   );
