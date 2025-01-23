05527186ba14323937b3cdada86da27e1597b460
sunshinevendetta <57574173+sunshinevendetta@users.noreply.github.com>
2025-01-23 14:36:30 -0600

    v1

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import WarningModal from './components/warningmodal';
 export default function IndexThree() {
   return (
     <div className="bg-black text-white flex justify-center items-center h-screen">
-      <WarningModal />
+
     </div>
   );
 }
