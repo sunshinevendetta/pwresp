4c61652a8ce68508df4cf4457e29e2d57ee7a266
sunshinevendetta <57574173+sunshinevendetta@users.noreply.github.com>
2025-01-28 19:48:14 -0600

    v1

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ import React from "react";
 export default function DisconnectedPage() {
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center bg-black text-white text-center p-6">
-      <h1 className="text-4xl font-bold">Este sitio esta desconectado</h1>
+      <h1 className="text-4xl font-bold">Este sitio esta desconectado por falta de pago y sentido de responsabilidad del CEO en DFB (Decentralized Future Builders)</h1>
       <p className="mt-4 text-lg">El contenido que buscas ha dejado de existir.</p>
     </div>
   );
